308b676a232769870d12ab4332d756c0a6310d44
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-10-10 22:10:18 +0300

    contact form completed

--- components/forms/contact-form.jsx
+++ components/forms/contact-form.jsx
@@ -1,45 +1,137 @@
-"use client";
-import { ReloadIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
-import { Button } from "../ui/button";
+'use client';
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
+import { Button } from '../ui/button';
+import axios from 'axios';
+import { useImmer } from 'use-immer';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { ExclamationTriangleIcon, CheckIcon } from '@radix-ui/react-icons';
 
 export default function ContactForm() {
-  const [isLoading, SetLoading] = useState(false);
-  return (
-    <form>
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="Your Name"
-          required
-          className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-        />
-      </div>
-      <div className="mb-4">
-        <input
-          type="email"
-          placeholder="Your Email"
-          required
-          className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-        />
-      </div>
-      <div className="mb-4">
-        <textarea
-          rows="6"
-          placeholder="Your Message"
-          className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-        ></textarea>
-      </div>
-      <div>
-        <Button
-          type="submit"
-          disabled={isLoading}
-          className="bg-primary border-primary w-full rounded border p-3 py-6 text-white transition hover:bg-opacity-90"
-        >
-          {isLoading && <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />}
-          Send Message
-        </Button>
-      </div>
-    </form>
-  );
+    const [isLoading, SetLoading] = useState(false);
+
+    const [isSuccess, SetSuccess] = useState(null);
+
+    const [data, updateData] = useImmer({
+        title: '',
+        email: '',
+        content: '',
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        SetLoading(true);
+        axios
+            .post('/api/contact', data)
+            .then((res) => {
+                if (res?.data?.success) {
+                    SetSuccess(true);
+                    SetLoading(false);
+                    updateData({
+                        title: '',
+                        email: '',
+                        content: '',
+                    });
+                    setTimeout(() => {
+                        SetSuccess(null);
+                    }, 3000);
+                } else {
+                    SetSuccess(false);
+                    SetLoading(false);
+                    setTimeout(() => {
+                        SetSuccess(null);
+                    }, 5000);
+                }
+            })
+            .catch((err) => {
+                SetSuccess(false);
+                SetLoading(false);
+                setTimeout(() => {
+                    SetSuccess(null);
+                }, 5000);
+            });
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="mb-4">
+                <input
+                    type="text"
+                    placeholder="Your Name"
+                    value={data.title}
+                    onChange={(e) => {
+                        updateData((draft) => {
+                            draft.title = e.target.value;
+                        });
+                    }}
+                    required
+                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                />
+            </div>
+            <div className="mb-4">
+                <input
+                    type="email"
+                    placeholder="Your Email"
+                    value={data.email}
+                    onChange={(e) => {
+                        updateData((draft) => {
+                            draft.email = e.target.value;
+                        });
+                    }}
+                    required
+                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                />
+            </div>
+            <div className="mb-4">
+                <textarea
+                    rows="6"
+                    placeholder="Your Message"
+                    value={data.content}
+                    onChange={(e) => {
+                        updateData((draft) => {
+                            draft.content = e.target.value;
+                        });
+                    }}
+                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                ></textarea>
+            </div>
+            <div>
+                <Button
+                    type="submit"
+                    disabled={isLoading}
+                    className="bg-primary border-primary w-full rounded border p-3 py-6 text-white transition hover:bg-opacity-90"
+                >
+                    {isLoading && (
+                        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Send Message
+                </Button>
+            </div>
+            {isSuccess !== null && (
+                <Alert
+                    className={`mt-2 rounded p-2 flex items-center ${
+                        isSuccess === true
+                            ? 'border-green-600 text-green-600'
+                            : ''
+                    } ${
+                        isSuccess === false ? 'border-red-600 text-red-600' : ''
+                    }`}
+                >
+                    <div>
+                        {isSuccess ? (
+                            <CheckIcon className="h-5 w-5" />
+                        ) : (
+                            <ExclamationTriangleIcon className="h-5 w-5" />
+                        )}
+                    </div>
+                    {/* <AlertTitle>Error</AlertTitle> */}
+                    <AlertDescription>
+                        {isSuccess
+                            ? 'Your message has been delivered'
+                            : 'Your message could not be delivered'}
+                    </AlertDescription>
+                </Alert>
+            )}
+        </form>
+    );
 }
--- prisma/seed.js
+++ prisma/seed.js
@@ -2,37 +2,38 @@ import prisma from '../lib/prisma'
 
 async function main() {
     const response = await Promise.all([
-        prisma.users.upsert({
-            where: { email: 'rauchg@vercel.com' },
-            update: {},
-            create: {
-                name: 'Guillermo Rauch',
-                email: 'rauchg@vercel.com',
-                image:
-                    'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
-            },
-        }),
-        prisma.users.upsert({
-            where: { email: 'lee@vercel.com' },
-            update: {},
-            create: {
-                name: 'Lee Robinson',
-                email: 'lee@vercel.com',
-                image:
-                    'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
-            },
-        }),
-        await prisma.users.upsert({
-            where: { email: 'm.kemalgordesli@gmail.com' },
-            update: {},
-            create: {
-                name: 'Mustafa',
-                email: 'stey@vercel.com',
-                image:
-                    'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
-            },
-        }),
+        // prisma.users.upsert({
+        //     where: { email: 'rauchg@vercel.com' },
+        //     update: {},
+        //     create: {
+        //         name: 'Guillermo Rauch',
+        //         email: 'rauchg@vercel.com',
+        //         image:
+        //             'https://pbs.twimg.com/profile_images/1576257734810312704/ucxb4lHy_400x400.jpg',
+        //     },
+        // }),
+        // prisma.users.upsert({
+        //     where: { email: 'lee@vercel.com' },
+        //     update: {},
+        //     create: {
+        //         name: 'Lee Robinson',
+        //         email: 'lee@vercel.com',
+        //         image:
+        //             'https://pbs.twimg.com/profile_images/1587647097670467584/adWRdqQ6_400x400.jpg',
+        //     },
+        // }),
+        // await prisma.users.upsert({
+        //     where: { email: 'm.kemalgordesli@gmail.com' },
+        //     update: {},
+        //     create: {
+        //         name: 'Mustafa',
+        //         email: 'stey@vercel.com',
+        //         image:
+        //             'https://pbs.twimg.com/profile_images/1506792347840888834/dS-r50Je_400x400.jpg',
+        //     },
+        // }),
     ])
+
     console.log(response)
 }
 main()
