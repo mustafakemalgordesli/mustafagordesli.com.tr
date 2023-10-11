0844b7204aa16d78c6aa2833f25f9a91e334a112
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-10-11 20:49:05 +0300

    google recaptha added

--- components/forms/contact-form.jsx
+++ components/forms/contact-form.jsx
@@ -6,11 +6,17 @@ import axios from 'axios';
 import { useImmer } from 'use-immer';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { ExclamationTriangleIcon, CheckIcon } from '@radix-ui/react-icons';
+import {
+    GoogleReCaptchaProvider,
+    GoogleReCaptcha,
+} from 'react-google-recaptcha-v3';
 
 export default function ContactForm() {
     const [isLoading, SetLoading] = useState(false);
 
     const [isSuccess, SetSuccess] = useState(null);
+
+    const [token, setToken] = useState();
 
     const [data, updateData] = useImmer({
         title: '',
@@ -53,85 +59,96 @@ export default function ContactForm() {
     };
 
     return (
-        <form onSubmit={onSubmit}>
-            <div className="mb-4">
-                <input
-                    type="text"
-                    placeholder="Your Name"
-                    value={data.title}
-                    onChange={(e) => {
-                        updateData((draft) => {
-                            draft.title = e.target.value;
-                        });
-                    }}
-                    required
-                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-                />
-            </div>
-            <div className="mb-4">
-                <input
-                    type="email"
-                    placeholder="Your Email"
-                    value={data.email}
-                    onChange={(e) => {
-                        updateData((draft) => {
-                            draft.email = e.target.value;
-                        });
-                    }}
-                    required
-                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-                />
-            </div>
-            <div className="mb-4">
-                <textarea
-                    rows="6"
-                    placeholder="Your Message"
-                    value={data.content}
-                    onChange={(e) => {
-                        updateData((draft) => {
-                            draft.content = e.target.value;
-                        });
-                    }}
-                    className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
-                ></textarea>
-            </div>
-            <div>
-                <Button
-                    type="submit"
-                    disabled={isLoading}
-                    className="bg-primary border-primary w-full rounded border p-3 py-6 text-white transition hover:bg-opacity-90"
-                >
-                    {isLoading && (
-                        <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                    )}
-                    Send Message
-                </Button>
-            </div>
-            {isSuccess !== null && (
-                <Alert
-                    className={`mt-2 rounded p-2 flex items-center ${
-                        isSuccess === true
-                            ? 'border-green-600 text-green-600'
-                            : ''
-                    } ${
-                        isSuccess === false ? 'border-red-600 text-red-600' : ''
-                    }`}
-                >
-                    <div>
-                        {isSuccess ? (
-                            <CheckIcon className="h-5 w-5" />
-                        ) : (
-                            <ExclamationTriangleIcon className="h-5 w-5" />
+        <>
+            <form onSubmit={onSubmit}>
+                <div className="mb-4">
+                    <input
+                        type="text"
+                        placeholder="Your Name"
+                        value={data.title}
+                        onChange={(e) => {
+                            updateData((draft) => {
+                                draft.title = e.target.value;
+                            });
+                        }}
+                        required
+                        className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        type="email"
+                        placeholder="Your Email"
+                        value={data.email}
+                        onChange={(e) => {
+                            updateData((draft) => {
+                                draft.email = e.target.value;
+                            });
+                        }}
+                        required
+                        className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                    />
+                </div>
+                <div className="mb-4">
+                    <textarea
+                        rows="6"
+                        placeholder="Your Message"
+                        value={data.content}
+                        onChange={(e) => {
+                            updateData((draft) => {
+                                draft.content = e.target.value;
+                            });
+                        }}
+                        className="text-body-color border-[f0f0f0] focus:border-primary w-full rounded border py-3 px-[14px] text-base outline-none focus-visible:shadow-none"
+                    ></textarea>
+                </div>
+                <div>
+                    <Button
+                        type="submit"
+                        disabled={isLoading}
+                        className="bg-primary border-primary w-full rounded border p-3 py-6 text-white transition hover:bg-opacity-90"
+                    >
+                        {isLoading && (
+                            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
                         )}
-                    </div>
-                    {/* <AlertTitle>Error</AlertTitle> */}
-                    <AlertDescription>
-                        {isSuccess
-                            ? 'Your message has been delivered'
-                            : 'Your message could not be delivered'}
-                    </AlertDescription>
-                </Alert>
-            )}
-        </form>
+                        Send Message
+                    </Button>
+                </div>
+                {isSuccess !== null && (
+                    <Alert
+                        className={`mt-2 rounded p-2 flex items-center ${
+                            isSuccess === true
+                                ? 'border-green-600 text-green-600'
+                                : ''
+                        } ${
+                            isSuccess === false
+                                ? 'border-red-600 text-red-600'
+                                : ''
+                        }`}
+                    >
+                        <div>
+                            {isSuccess ? (
+                                <CheckIcon className="h-5 w-5" />
+                            ) : (
+                                <ExclamationTriangleIcon className="h-5 w-5" />
+                            )}
+                        </div>
+                        {/* <AlertTitle>Error</AlertTitle> */}
+                        <AlertDescription>
+                            {isSuccess
+                                ? 'Your message has been delivered'
+                                : 'Your message could not be delivered'}
+                        </AlertDescription>
+                    </Alert>
+                )}
+            </form>
+            <GoogleReCaptchaProvider reCaptchaKey="6LepPpMoAAAAACJpRwkIz5wlEZ7EolHUtjKdl7a-">
+                <GoogleReCaptcha
+                    onVerify={(token) => {
+                        setToken(token);
+                    }}
+                />
+            </GoogleReCaptchaProvider>
+        </>
     );
 }
